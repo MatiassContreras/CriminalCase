b3e706066c162e640a816f17c9c87d2e43dd3f6e
MatiassContreras <MatiHD4K@outlook.com.ar>
2023-10-10 02:27:34 -0300

    agregandole authcontext a home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
 // Home.js
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { useAuth } from '../context/authContext';
 
 const HomeScreen = ({ navigation }) => {
+  const {user, logout , loading} =useAuth();
+  const handleLogout = async()=> {
+      await logout();
+  }    
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>¡Bienvenido a la pantalla de inicio!</Text>
       <Button title="Iniciar sesión" onPress={() => navigation.navigate('Profile')} />
+      <Button title="Logout" onPress={(handleLogout)} />
     </View>
   );
 };
